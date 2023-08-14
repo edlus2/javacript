af1f2445393bb7c2ebce87cd8137ff36189ec2ef
edlus2 <edgar.ludis2@gmail.com>
2023-08-14 16:11:05 -0300

    att

--- aulas/Módulo E/ambiente.js
+++ aulas/Módulo E/ambiente.js
@@ -1,1 +1,19 @@
-console.log('tudo bem?')
+var c = 1
+
+do{
+    console.log(`passo ${c}`)
+    c++
+}
+while (c <= 6)
+
+/*while (c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}
+
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')*/
